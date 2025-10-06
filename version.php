ed8a30f7edfd4f6f35bf9c67f896175029e199e1
Derick Turner <derick@e-learndesign.co.uk>
2025-10-06 10:58:54 +0100

    IOMAD: 4.5.7 release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->release  = '4.5.6 (Build: 20250811)'; // Human-friendly version name.
+$plugin->release  = '4.5.7 (Build: 20251006)'; // Human-friendly version name.
 $plugin->version  = 2025012345;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2024100700;   // Requires this Moodle version.
 $plugin->component  = 'mod_trainingevent';
